81460b39b10923f7298c6489e945c58c281b2858
aleksandercieslak <aleks.cieslak@proton.me>
2026-07-15 10:19:50 +0200

    Tweak Neovim C++ compilation keybind. Compile to /tmp/.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() 
 vim.keymap.set("n", "<F5>", function()
   vim.cmd("write")
   local file = vim.fn.expand("%:p")
-  local out = vim.fn.expand("%:p:r")
+  local out = "/tmp/" .. name 
   local cmd = "g++ -std=c++17 -O2 -Wall -Wextra -Wshadow -fsanitize=address,undefined -DLOCAL " .. file .. " -o " .. out .. " && " .. out
   require("toggleterm").exec(cmd)
 end, {})
